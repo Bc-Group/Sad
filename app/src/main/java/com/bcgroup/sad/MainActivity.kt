dde01591077871de39b3067bfd93bffb23d7a112
Shakil Patel <patelsakib95@gmail.com>
2022-12-10 11:46:22 +0530

    initial commit

--- app/src/main/java/com/bcgroup/sad/MainActivity.kt
+++ app/src/main/java/com/bcgroup/sad/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bcgroup.sad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bcgroup.sad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
